c7fe9dd78c923797fcf42e0b7a7e7867f8d70a68
hazedrops <hazedrops@hotmail.com>
2022-10-20 22:28:48 -0700

    Bootstrap Landing Page Practice

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// const year = document.getElementById('year');
+
+// // Get the current year for the copyright
+// const d = new Date(); page.currentYear = d.getFullYear(); 
+// year.innerHTML = d;
